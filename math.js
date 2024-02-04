d14c4f4a6b99c3c5d2642f473ea170303f5d4aee
bidduthosen <mdbidduthosen34@gmail.com>
2024-02-04 15:31:57 +0600

    math in javascript

---
+++ math.js
@@ -0,0 +1,13 @@
+let numbers = 4;
+let numbers2 = 6
+let math = Math.min(numbers, numbers2);
+let maxMath = Math.max(numbers, numbers2);
+// console.log(math)    
+// console.log('max', maxMath)
+// console.log(Math.round(3.49))   
+// console.log(Math.round(3.563))  
+// console.log(Math.ceil(66.22)) 
+// console.log(Math.floor(99.899));
+
+// Random generate number ======================
+console.log(Math.round(Math.random()*20))
